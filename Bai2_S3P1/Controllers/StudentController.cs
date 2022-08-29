ecd01ae35a33aad235e62c00a49fcd53b951202f
duc-canh <duccanh95hptl@gmail.com>
2022-08-29 20:08:24 +0700

    Add project files.

---
+++ Bai2_S3P1/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2_S3P1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Nguyễn Văn A";
+            TempData["Age"] = "18";
+            TempData["Address"] = "Hà Nội";
+            return Redirect("Student/ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            return View("ShowData");
+        }
+    }
+}
---
+++ Bai2_S3P1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bai2_S3P1.MvcApplication" Language="C#" %>
